b68875a537e32570556a53bdc1b7c0378f1812a6
zhanxw <zhanxw@users.noreply.github.com>
2017-03-30 01:40:35 -0500

    Update cmd.sh

--- example/cmd.sh
+++ example/cmd.sh
@@ -1,1 +1,1 @@
-xtime ../../executable/ancestrySeq --inSeq 1108.amd.to.hgdp.seq.1.gz --inSite HGDP_938.site  --inModel spa.model.out --out output.txt
+../executable/ancestrySeq --inSeq 1108.amd.to.hgdp.seq.1.gz --inSite HGDP_938.site  --inModel spa.model.out --out output.txt
